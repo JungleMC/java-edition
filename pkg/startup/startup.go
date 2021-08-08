c52ab6e48c476698a6027efae40aac45f91de35d
Ella Fox <ella@fox.gal>
2021-08-08 12:02:57 +0200

    Bootstrap elsewhere

---
+++ pkg/startup/startup.go
@@ -0,0 +1,19 @@
+package startup
+
+import (
+	"github.com/JungleMC/java-edition/internal/config"
+	"github.com/JungleMC/java-edition/internal/net"
+	"github.com/caarlos0/env"
+)
+
+func Start() {
+	config.Get = &config.Config{}
+	if err := env.Parse(config.Get); err != nil {
+		panic(err)
+	}
+
+	_, err := net.Bootstrap(config.Get.ListenAddress, config.Get.ListenPort, config.Get.OnlineMode)
+	if err != nil {
+		panic(err)
+	}
+}
